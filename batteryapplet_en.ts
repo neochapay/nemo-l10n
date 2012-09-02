e1cc59639b78aa7ee76d6d87f3bd6419b0e05615
Sergey Chupligin <neochapay@gmail.com>
2012-09-02 14:15:28 +0400

    Update battey applet files

--- batteryapplet_en.ts
+++ batteryapplet_en.ts
@@ -1,96 +1,188 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="3.0" language="en_GB">
+<TS version="2.0" language="en_GB">
 <context>
     <name></name>
     <message>
         <source>qtn_ener_condition_poor</source>
-        <translation>Poor</translation>
+        <translation type="obsolete">Poor</translation>
     </message>
     <message numerus="yes">
         <source>qtn_comm_time_minute</source>
-        <translation>
+        <translation type="obsolete">
             <numerusform>%1 minute</numerusform>
             <numerusform>%1 minutes</numerusform>
         </translation>
     </message>
     <message>
         <source>qtn_ener_condition_good</source>
-        <translation>Good</translation>
+        <translation type="obsolete">Good</translation>
     </message>
     <message numerus="yes">
         <source>qtn_comm_time_hour</source>
-        <translation>
+        <translation type="obsolete">
             <numerusform>%1 hour</numerusform>
             <numerusform>%1 hours</numerusform>
         </translation>
     </message>
     <message>
         <source>qtn_ener_autops</source>
-        <translation>Automatic</translation>
+        <translation type="obsolete">Automatic</translation>
     </message>
     <message>
         <source>qtn_ener_power_save_mode</source>
-        <translation>Power save mode</translation>
+        <translation type="obsolete">Power save mode</translation>
     </message>
     <message>
         <source>qtn_ener_condition_poor_info</source>
-        <translation>The battery capacity has decreased. You can </translation>
+        <translation type="obsolete">The battery capacity has decreased. You can </translation>
     </message>
     <message>
         <source>qtn_ener_charging</source>
-        <translation>Charging</translation>
+        <translation type="obsolete">Charging</translation>
     </message>
     <message>
         <source>qtn_ener_charcomp</source>
-        <translation>Charging complete</translation>
+        <translation type="obsolete">Charging complete</translation>
     </message>
     <message>
         <source>qtn_ener_power_save_combo</source>
-        <translation>Power Save Mode</translation>
+        <translation type="obsolete">Power Save Mode</translation>
     </message>
     <message>
         <source>qtn_ener_activation</source>
-        <translation>Activation</translation>
+        <translation type="obsolete">Activation</translation>
     </message>
     <message>
         <source>qtn_ener_battery_level</source>
-        <translation>Battery level %L1</translation>
+        <translation type="obsolete">Battery level %L1</translation>
     </message>
     <message>
         <source>qtn_ener_battery_condition</source>
-        <translation>Battery condition</translation>
+        <translation type="obsolete">Battery condition</translation>
     </message>
     <message numerus="yes">
         <source>qtn_comm_time_day</source>
-        <translation>
+        <translation type="obsolete">
             <numerusform>%1 day</numerusform>
             <numerusform>%1 days</numerusform>
         </translation>
     </message>
     <message>
         <source>qtn_comm_settings_off</source>
-        <translation>Off</translation>
+        <translation type="obsolete">Off</translation>
     </message>
     <message>
         <source>qtn_comm_settings_on</source>
-        <translation>On</translation>
+        <translation type="obsolete">On</translation>
     </message>
     <message>
         <source>qtn_ener_remaining_standby</source>
-        <translation>Remaining standby time: %1</translation>
+        <translation type="obsolete">Remaining standby time: %1</translation>
     </message>
     <message>
         <source>qtn_ener_charging_estimate</source>
-        <translation>Remaining</translation>
+        <translation type="obsolete">Remaining</translation>
     </message>
     <message>
         <source>qtn_ener_remaining_talk</source>
-        <translation>Remaining talk time: %1</translation>
+        <translation type="obsolete">Remaining talk time: %1</translation>
     </message>
     <message>
         <source>qtn_ener_battery</source>
-        <translation>Battery</translation>
+        <translation type="obsolete">Battery</translation>
+    </message>
+    <message id="qtn_ener_battery">
+        <source>Battery</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_power_save_combo">
+        <source>Power Save Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_settings_off">
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_settings_on">
+        <source>On</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_autops">
+        <source>Automatic</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_battery_condition">
+        <source>Battery condition</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_condition_poor_info">
+        <source>The battery capacity has decreased. You can </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_charcomp">
+        <source>Charging complete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_power_save_mode">
+        <source>Power save mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_charging">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_condition_poor">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_condition_good">
+        <source>Good</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_battery_level">
+        <source>Battery level %L1%</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_charging_estimate" numerus="yes">
+        <source></source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="qtn_ener_remaining_talk">
+        <source>Remaining talk time: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_remaining_standby">
+        <source>Remaining standby time: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_time_day" numerus="yes">
+        <source>%1 day &lt;%1 days&gt;</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="qtn_comm_time_hour" numerus="yes">
+        <source>%1 hour &lt;%1 hours&gt;</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="qtn_comm_time_minute" numerus="yes">
+        <source>%1 minute &lt;%1 minutes&gt;</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="qtn_ener_activation">
+        <source></source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- batteryapplet_ru.ts
+++ batteryapplet_ru.ts
@@ -5,72 +5,69 @@
     <name></name>
     <message>
         <source>qtn_ener_condition_poor</source>
-        <translation type="unfinished">Плохо</translation>
+        <translation type="obsolete">Плохо</translation>
     </message>
     <message numerus="yes">
         <source>qtn_comm_time_minute</source>
-        <translation type="unfinished">
+        <translation type="obsolete">
             <numerusform>%1 минута</numerusform>
             <numerusform>%1 минуты</numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
     <message>
         <source>qtn_ener_condition_good</source>
-        <translation type="unfinished">Хорошо</translation>
+        <translation type="obsolete">Хорошо</translation>
     </message>
     <message numerus="yes">
         <source>qtn_comm_time_hour</source>
-        <translation type="unfinished">
+        <translation type="obsolete">
             <numerusform>%1 час</numerusform>
             <numerusform>%1 часов</numerusform>
+            <numerusform></numerusform>
         </translation>
     </message>
     <message>
         <source>qtn_ener_autops</source>
-        <translation type="unfinished">Автомат</translation>
+        <translation type="obsolete">Автомат</translation>
     </message>
     <message>
         <source>qtn_ener_power_save_mode</source>
-        <translation>Режим энергосбережения</translation>
+        <translation type="obsolete">Режим энергосбережения</translation>
     </message>
     <message>
         <source>qtn_ener_condition_poor_info</source>
         <translatorcomment>The battery capacity has decreased. You can</translatorcomment>
-        <translation>Ёмкость батареи уменьшилась. Вы можете</translation>
+        <translation type="obsolete">Ёмкость батареи уменьшилась. Вы можете</translation>
     </message>
     <message>
         <source>qtn_ener_charging</source>
         <translatorcomment>Charging</translatorcomment>
-        <translation>Заряжается</translation>
+        <translation type="obsolete">Заряжается</translation>
     </message>
     <message>
         <source>qtn_ener_charcomp</source>
         <translatorcomment>Charging complete</translatorcomment>
-        <translation>Зарядка завершена</translation>
+        <translation type="obsolete">Зарядка завершена</translation>
     </message>
     <message>
         <source>qtn_ener_power_save_combo</source>
         <translatorcomment>Power Save Mode</translatorcomment>
-        <translation>Режим экономии энергии</translation>
-    </message>
-    <message>
-        <source>qtn_ener_activation</source>
-        <translatorcomment>Activation</translatorcomment>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">Режим экономии энергии</translation>
     </message>
     <message>
         <source>qtn_ener_battery_level</source>
         <translatorcomment>Battery level %L1</translatorcomment>
-        <translation>Уровень заряда %L1</translation>
+        <translation type="obsolete">Уровень заряда %L1</translation>
     </message>
     <message>
         <source>qtn_ener_battery_condition</source>
         <translatorcomment>Battery condition</translatorcomment>
-        <translation>Состояние батареи</translation>
+        <translation type="obsolete">Состояние батареи</translation>
     </message>
     <message numerus="yes">
         <source>qtn_comm_time_day</source>
-        <translation>
+        <translation type="obsolete">
             <numerusform>%1 день</numerusform>
             <numerusform>%1 дни</numerusform>
             <numerusform></numerusform>
@@ -79,32 +76,126 @@
     <message>
         <source>qtn_comm_settings_off</source>
         <translatorcomment>Off</translatorcomment>
-        <translation>Выкл</translation>
+        <translation type="obsolete">Выкл</translation>
     </message>
     <message>
         <source>qtn_comm_settings_on</source>
         <translatorcomment>On</translatorcomment>
-        <translation>Вкл</translation>
+        <translation type="obsolete">Вкл</translation>
     </message>
     <message>
         <source>qtn_ener_remaining_standby</source>
         <translatorcomment>Remaining standby time: %1</translatorcomment>
-        <translation>Время работы в режиме ожидания: %1</translation>
+        <translation type="obsolete">Время работы в режиме ожидания: %1</translation>
     </message>
     <message>
         <source>qtn_ener_charging_estimate</source>
         <translatorcomment>Remaining</translatorcomment>
-        <translation>Осталось</translation>
+        <translation type="obsolete">Осталось</translation>
     </message>
     <message>
         <source>qtn_ener_remaining_talk</source>
         <translatorcomment>Remaining talk time: %1</translatorcomment>
-        <translation>Время работы в режиме разговоров: %1</translation>
+        <translation type="obsolete">Время работы в режиме разговоров: %1</translation>
     </message>
     <message>
         <source>qtn_ener_battery</source>
         <translatorcomment>Battery</translatorcomment>
+        <translation type="obsolete">Батарея</translation>
+    </message>
+    <message id="qtn_ener_battery">
+        <source>Battery</source>
         <translation>Батарея</translation>
+    </message>
+    <message id="qtn_ener_power_save_combo">
+        <source>Power Save Mode</source>
+        <translation>Режим энергосбережения</translation>
+    </message>
+    <message id="qtn_comm_settings_off">
+        <source>Off</source>
+        <translation>Выкл</translation>
+    </message>
+    <message id="qtn_comm_settings_on">
+        <source>On</source>
+        <translation>Вкл</translation>
+    </message>
+    <message id="qtn_ener_autops">
+        <source>Automatic</source>
+        <translation>Автоматически</translation>
+    </message>
+    <message id="qtn_ener_battery_condition">
+        <source>Battery condition</source>
+        <translation>Состояние батареи</translation>
+    </message>
+    <message id="qtn_ener_condition_poor_info">
+        <source>The battery capacity has decreased. You can </source>
+        <translation>Батарея потеряла ёмкость. Вы можете</translation>
+    </message>
+    <message id="qtn_ener_charcomp">
+        <source>Charging complete</source>
+        <translation>Зарядка завершена</translation>
+    </message>
+    <message id="qtn_ener_power_save_mode">
+        <source>Power save mode</source>
+        <translation>Режим энергосбережения</translation>
+    </message>
+    <message id="qtn_ener_charging">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_condition_poor">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_ener_condition_good">
+        <source>Good</source>
+        <translation>Хорошо</translation>
+    </message>
+    <message id="qtn_ener_battery_level">
+        <source>Battery level %L1%</source>
+        <translation>Уровень заряда %L1%</translation>
+    </message>
+    <message id="qtn_ener_charging_estimate" numerus="yes">
+        <source></source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="qtn_ener_remaining_talk">
+        <source>Remaining talk time: %1</source>
+        <translation type="unfinished">Примерное время в режиме разговора: %1</translation>
+    </message>
+    <message id="qtn_ener_remaining_standby">
+        <source>Remaining standby time: %1</source>
+        <translation>Время работы в режиме ожидания: %1</translation>
+    </message>
+    <message id="qtn_comm_time_day" numerus="yes">
+        <source>%1 day &lt;%1 days&gt;</source>
+        <translation>
+            <numerusform>%n день</numerusform>
+            <numerusform>%n дня</numerusform>
+            <numerusform>%n дней</numerusform>
+        </translation>
+    </message>
+    <message id="qtn_comm_time_hour" numerus="yes">
+        <source>%1 hour &lt;%1 hours&gt;</source>
+        <translation>
+            <numerusform>%n час</numerusform>
+            <numerusform>%n часа</numerusform>
+            <numerusform>%n часов</numerusform>
+        </translation>
+    </message>
+    <message id="qtn_comm_time_minute" numerus="yes">
+        <source>%1 minute &lt;%1 minutes&gt;</source>
+        <translation>
+            <numerusform>%n минута</numerusform>
+            <numerusform>%n минуты</numerusform>
+            <numerusform>%n минут</numerusform>
+        </translation>
+    </message>
+    <message id="qtn_ener_activation">
+        <source></source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
