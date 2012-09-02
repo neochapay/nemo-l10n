f573dab3c3d5dbcc5a7f4969993d4a9c1700e91b
Sergey Chupligin <neochapay@gmail.com>
2012-09-02 14:21:36 +0400

    Update display applet files

--- displayapplet_en.ts
+++ displayapplet_en.ts
@@ -5,39 +5,97 @@
     <name></name>
     <message>
         <source>qtn_comm_time_minute</source>
-        <translation>%1 minutes</translation>
+        <translation type="obsolete">%1 minutes</translation>
     </message>
     <message>
         <source>qtn_disp_display</source>
-        <translation>Display</translation>
+        <translation type="obsolete">Display</translation>
     </message>
     <message>
         <source>qtn_disp_bright</source>
-        <translation>Brightness</translation>
+        <translation type="obsolete">Brightness</translation>
     </message>
     <message>
         <source>qtn_ener_power_save_mode</source>
-        <translation>In power save mode</translation>
+        <translation type="obsolete">In power save mode</translation>
     </message>
     <message>
         <source>qtn_comm_time_second</source>
-        <translation>%1 seconds</translation>
+        <translation type="obsolete">%1 seconds</translation>
     </message>
     <message>
         <source>qtn_disp_doubletap</source>
-        <translation>Double tap wake-up</translation>
+        <translation type="obsolete">Double tap wake-up</translation>
     </message>
     <message>
         <source>qtn_comm_swipe_to_close</source>
-        <translation>Swipe from top to close</translation>
+        <translation type="obsolete">Swipe from top to close</translation>
     </message>
     <message>
         <source>qtn_disp_lowpower</source>
-        <translation>Low power mode</translation>
+        <translation type="obsolete">Low power mode</translation>
     </message>
     <message>
         <source>qtn_disp_screenoff</source>
-        <translation>Backlight time out</translation>
+        <translation type="obsolete">Backlight time out</translation>
+    </message>
+    <message id="qtn_ener_power_save_mode">
+        <source>In power save mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_display">
+        <source>Display</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_profile_normal">
+        <source>Normal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_profile_vivid">
+        <source>Vivid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_profile_muted">
+        <source>Muted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_bright">
+        <source>Brightness</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_screenoff">
+        <source>Backlight time out</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_profile">
+        <source>Color profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_lowpower">
+        <source>Low power mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_disp_doubletap">
+        <source>Double tap wake-up</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_swipe_to_close">
+        <source>Swipe from top to close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_time_minute" numerus="yes">
+        <source>%1 minutes</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="qtn_comm_time_second" numerus="yes">
+        <source>%1 seconds</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
     </message>
 </context>
 </TS>
--- displayapplet_ru.ts
+++ displayapplet_ru.ts
@@ -6,47 +6,107 @@
     <message>
         <source>qtn_comm_time_minute</source>
         <translatorcomment>%1 minutes</translatorcomment>
-        <translation>%1 минут</translation>
+        <translation type="obsolete">%1 минут</translation>
     </message>
     <message>
         <source>qtn_disp_display</source>
         <translatorcomment>Display</translatorcomment>
-        <translation>Экран</translation>
+        <translation type="obsolete">Экран</translation>
     </message>
     <message>
         <source>qtn_disp_bright</source>
         <translatorcomment>Brightness</translatorcomment>
-        <translation>Яркость</translation>
+        <translation type="obsolete">Яркость</translation>
     </message>
     <message>
         <source>qtn_ener_power_save_mode</source>
         <translatorcomment>In power save mode</translatorcomment>
-        <translation>В режиме сохранения энергии</translation>
+        <translation type="obsolete">В режиме сохранения энергии</translation>
     </message>
     <message>
         <source>qtn_comm_time_second</source>
         <translatorcomment>%1 seconds</translatorcomment>
-        <translation>%1 секунд</translation>
+        <translation type="obsolete">%1 секунд</translation>
     </message>
     <message>
         <source>qtn_disp_doubletap</source>
         <translatorcomment>Double tap wake-up</translatorcomment>
-        <translation>Double tap wake-up</translation>
+        <translation type="obsolete">Double tap wake-up</translation>
     </message>
     <message>
         <source>qtn_comm_swipe_to_close</source>
         <translatorcomment>Swipe from top to close</translatorcomment>
-        <translation>Сдвинуть вверх для закрытия</translation>
+        <translation type="obsolete">Сдвинуть вверх для закрытия</translation>
     </message>
     <message>
         <source>qtn_disp_lowpower</source>
         <translatorcomment>Low power mode</translatorcomment>
-        <translation>Режим энергосбережения</translation>
+        <translation type="obsolete">Режим энергосбережения</translation>
     </message>
     <message>
         <source>qtn_disp_screenoff</source>
         <translatorcomment>Backlight time out</translatorcomment>
-        <translation>Время выключения подсветки</translation>
+        <translation type="obsolete">Время выключения подсветки</translation>
+    </message>
+    <message id="qtn_ener_power_save_mode">
+        <source>In power save mode</source>
+        <translation>В режиме сохранения энергии</translation>
+    </message>
+    <message id="qtn_disp_display">
+        <source>Display</source>
+        <translation>Экран</translation>
+    </message>
+    <message id="qtn_disp_profile_normal">
+        <source>Normal</source>
+        <translation>Yjhvfkmysq</translation>
+    </message>
+    <message id="qtn_disp_profile_vivid">
+        <source>Vivid</source>
+        <translation>Яркий</translation>
+    </message>
+    <message id="qtn_disp_profile_muted">
+        <source>Muted</source>
+        <translation>приглушенный</translation>
+    </message>
+    <message id="qtn_disp_bright">
+        <source>Brightness</source>
+        <translation>Яркость</translation>
+    </message>
+    <message id="qtn_disp_screenoff">
+        <source>Backlight time out</source>
+        <translation>Время работы подсветки</translation>
+    </message>
+    <message id="qtn_disp_profile">
+        <source>Color profile</source>
+        <translation>Цветовой профиль</translation>
+    </message>
+    <message id="qtn_disp_lowpower">
+        <source>Low power mode</source>
+        <translation>Режим энергосбережения</translation>
+    </message>
+    <message id="qtn_disp_doubletap">
+        <source>Double tap wake-up</source>
+        <translation>Двойное касание для пробуждения</translation>
+    </message>
+    <message id="qtn_comm_swipe_to_close">
+        <source>Swipe from top to close</source>
+        <translation>Сдвинуть вверх для закрытия</translation>
+    </message>
+    <message id="qtn_comm_time_minute" numerus="yes">
+        <source>%1 minutes</source>
+        <translation>
+            <numerusform>%1 минута</numerusform>
+            <numerusform>%1 минуты</numerusform>
+            <numerusform>%1 минут</numerusform>
+        </translation>
+    </message>
+    <message id="qtn_comm_time_second" numerus="yes">
+        <source>%1 seconds</source>
+        <translation>
+            <numerusform>%1 секунда</numerusform>
+            <numerusform>%1 секунды</numerusform>
+            <numerusform>%1 секунд</numerusform>
+        </translation>
     </message>
 </context>
 </TS>
