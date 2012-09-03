3f5a37a9836de4a850cbaf15483b0b793d948f91
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 10:32:47 +0400

    Add test applet files

---
+++ testapplet_en.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name></name>
+    <message id="qtn_test_test">
+        <location filename="testapplet.cpp" line="142"/>
+        <source>Test</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_userguide">
+        <location filename="testapplet.cpp" line="150"/>
+        <source>User guide</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ testapplet_ru.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message id="qtn_test_test">
+        <location filename="testapplet.cpp" line="142"/>
+        <source>Test</source>
+        <translation>Тест</translation>
+    </message>
+    <message id="qtn_comm_userguide">
+        <location filename="testapplet.cpp" line="150"/>
+        <source>User guide</source>
+        <translation>Руководство пользователя</translation>
+    </message>
+</context>
+</TS>
