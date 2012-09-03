7079ed00278d5a041f6dce47dffa3c57defdc567
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 10:40:52 +0400

    Update wallpapper2 applet files

--- wallpaperapplet2_en.ts
+++ wallpaperapplet2_en.ts
@@ -4,20 +4,28 @@
 <context>
     <name></name>
     <message>
-        <source>qtn_comm_cancel</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>qtn_wall_wallpaper</source>
-        <translation>Wallpaper</translation>
-    </message>
-    <message>
-        <source>qtn_comm_command_done</source>
-        <translation></translation>
+        <translation type="obsolete">Wallpaper</translation>
     </message>
     <message>
         <source>qtn_comm_save</source>
-        <translation>Save</translation>
+        <translation type="obsolete">Save</translation>
+    </message>
+    <message id="qtn_wall_wallpaper">
+        <source>Wallpaper</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_command_done">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_cancel">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_save">
+        <source>Save</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- wallpaperapplet2_ru.ts
+++ wallpaperapplet2_ru.ts
@@ -4,21 +4,29 @@
 <context>
     <name></name>
     <message>
-        <source>qtn_comm_cancel</source>
-        <translation></translation>
-    </message>
-    <message>
         <source>qtn_wall_wallpaper</source>
         <translatorcomment>Wallpaper</translatorcomment>
-        <translation>Обои</translation>
-    </message>
-    <message>
-        <source>qtn_comm_command_done</source>
-        <translation></translation>
+        <translation type="obsolete">Обои</translation>
     </message>
     <message>
         <source>qtn_comm_save</source>
         <translatorcomment>Save</translatorcomment>
+        <translation type="obsolete">Сохранить</translation>
+    </message>
+    <message id="qtn_wall_wallpaper">
+        <source>Wallpaper</source>
+        <translation>Обои</translation>
+    </message>
+    <message id="qtn_comm_command_done">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_cancel">
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_save">
+        <source>Save</source>
         <translation>Сохранить</translation>
     </message>
 </context>
