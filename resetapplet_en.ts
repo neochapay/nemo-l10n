61039cae543dd3ffadfd15ee23e8449d9d868cee
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 10:15:39 +0400

    Update reset applet files

--- resetapplet_en.ts
+++ resetapplet_en.ts
@@ -5,35 +5,67 @@
     <name></name>
     <message>
         <source>qtn_rset_clear</source>
-        <translation>Clear device</translation>
+        <translation type="obsolete">Clear device</translation>
     </message>
     <message>
         <source>qtn_rset_not_possible</source>
-        <translation>Device resets are not possible while USB is connected in mass storage mode.</translation>
+        <translation type="obsolete">Device resets are not possible while USB is connected in mass storage mode.</translation>
     </message>
     <message>
         <source>qtn_rset_restore_query</source>
-        <translation>Restore original settings?</translation>
+        <translation type="obsolete">Restore original settings?</translation>
     </message>
     <message>
         <source>qtn_rset_clear_query</source>
-        <translation>Clear all user data and restore original settings?</translation>
+        <translation type="obsolete">Clear all user data and restore original settings?</translation>
     </message>
     <message>
         <source>qtn_rset_reset_settings</source>
-        <translation>Reset settings</translation>
+        <translation type="obsolete">Reset settings</translation>
     </message>
     <message>
         <source>qtn_rset_clear_query_title</source>
-        <translation>Clear all data?</translation>
+        <translation type="obsolete">Clear all data?</translation>
     </message>
     <message>
         <source>qtn_rset_restore_query_title</source>
-        <translation>Restore original settings?</translation>
+        <translation type="obsolete">Restore original settings?</translation>
     </message>
     <message>
         <source>qtn_rset_restore</source>
-        <translation>Restore original settings</translation>
+        <translation type="obsolete">Restore original settings</translation>
+    </message>
+    <message id="qtn_rset_reset_settings">
+        <source>Reset settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_rset_restore">
+        <source>Restore original settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_rset_clear">
+        <source>Clear device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_rset_restore_query">
+        <source>Restore original settings?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_rset_restore_query_title">
+        <source>Restore original settings?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_rset_clear_query">
+        <source>Clear all user data and restore original settings?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_rset_clear_query_title">
+        <source>Clear all data?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_rset_not_possible">
+        <source>Device resets are not possible while USB is connected in mass storage mode.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- resetapplet_ru.ts
+++ resetapplet_ru.ts
@@ -6,42 +6,74 @@
     <message>
         <source>qtn_rset_clear</source>
         <translatorcomment>Clear device</translatorcomment>
-        <translation>Отчистить устройство</translation>
+        <translation type="obsolete">Отчистить устройство</translation>
     </message>
     <message>
         <source>qtn_rset_not_possible</source>
         <translatorcomment>Device resets are not possible while USB is connected in mass storage mode.</translatorcomment>
-        <translation>Сброс устройства невозможен когда USB подключён в режиме внешнего диска.</translation>
+        <translation type="obsolete">Сброс устройства невозможен когда USB подключён в режиме внешнего диска.</translation>
     </message>
     <message>
         <source>qtn_rset_restore_query</source>
         <translatorcomment>Restore original settings?</translatorcomment>
-        <translation>Востановить заводские настройки?</translation>
+        <translation type="obsolete">Востановить заводские настройки?</translation>
     </message>
     <message>
         <source>qtn_rset_clear_query</source>
         <translatorcomment>Clear all user data and restore original settings?</translatorcomment>
-        <translation>Удалить все пользовательские данные и востановить заводские настройки?</translation>
+        <translation type="obsolete">Удалить все пользовательские данные и востановить заводские настройки?</translation>
     </message>
     <message>
         <source>qtn_rset_reset_settings</source>
         <translatorcomment>Reset settings</translatorcomment>
-        <translation>Сбросить настройки</translation>
+        <translation type="obsolete">Сбросить настройки</translation>
     </message>
     <message>
         <source>qtn_rset_clear_query_title</source>
         <translatorcomment>Clear all data?</translatorcomment>
-        <translation>Удалить все данные?</translation>
+        <translation type="obsolete">Удалить все данные?</translation>
     </message>
     <message>
         <source>qtn_rset_restore_query_title</source>
         <translatorcomment>Restore original settings?</translatorcomment>
-        <translation>Востановить заводские настройки?</translation>
+        <translation type="obsolete">Востановить заводские настройки?</translation>
     </message>
     <message>
         <source>qtn_rset_restore</source>
         <translatorcomment>Restore original settings</translatorcomment>
+        <translation type="obsolete">Востановить заводские настройки</translation>
+    </message>
+    <message id="qtn_rset_reset_settings">
+        <source>Reset settings</source>
+        <translation>Сбросить настройки</translation>
+    </message>
+    <message id="qtn_rset_restore">
+        <source>Restore original settings</source>
         <translation>Востановить заводские настройки</translation>
+    </message>
+    <message id="qtn_rset_clear">
+        <source>Clear device</source>
+        <translation>Отчистить устройство</translation>
+    </message>
+    <message id="qtn_rset_restore_query">
+        <source>Restore original settings?</source>
+        <translation>Востановить заводские настройки?</translation>
+    </message>
+    <message id="qtn_rset_restore_query_title">
+        <source>Restore original settings?</source>
+        <translation>Востановить заводские настройки?</translation>
+    </message>
+    <message id="qtn_rset_clear_query">
+        <source>Clear all user data and restore original settings?</source>
+        <translation>Удалить все пользовательские данные и востановить оригинальные настройки ?</translation>
+    </message>
+    <message id="qtn_rset_clear_query_title">
+        <source>Clear all data?</source>
+        <translation>Удалить всю информацию?</translation>
+    </message>
+    <message id="qtn_rset_not_possible">
+        <source>Device resets are not possible while USB is connected in mass storage mode.</source>
+        <translation>Сброс устройства невозвожен когда USB присоединён в режиме внешнего диска.</translation>
     </message>
 </context>
 </TS>
