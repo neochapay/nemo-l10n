8aa97cfd999a2c47098052231683461b72cb1ed7
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 10:12:20 +0400

    Update offline applet files

--- offlineapplet_en.ts
+++ offlineapplet_en.ts
@@ -5,27 +5,51 @@
     <name></name>
     <message>
         <source>qtn_offl_entering</source>
-        <translation>Closing all connections. Switching to offline mode.</translation>
+        <translation type="obsolete">Closing all connections. Switching to offline mode.</translation>
     </message>
     <message>
         <source>qtn_offl_exiting_title</source>
-        <translation>Exit offline mode?</translation>
+        <translation type="obsolete">Exit offline mode?</translation>
     </message>
     <message>
         <source>qtn_sett_main_flightmode</source>
-        <translation>Flight mode</translation>
+        <translation type="obsolete">Flight mode</translation>
     </message>
     <message>
         <source>qtn_offl_exiting</source>
-        <translation>Connections will be restored.</translation>
+        <translation type="obsolete">Connections will be restored.</translation>
     </message>
     <message>
         <source>qtn_offl_deactivate</source>
-        <translation>Deactivate offline mode</translation>
+        <translation type="obsolete">Deactivate offline mode</translation>
     </message>
     <message>
         <source>qtn_offl_activate</source>
-        <translation>Activate offline mode</translation>
+        <translation type="obsolete">Activate offline mode</translation>
+    </message>
+    <message id="qtn_sett_main_flightmode">
+        <source>Flight mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_offl_deactivate">
+        <source>Deactivate offline mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_offl_activate">
+        <source>Activate offline mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_offl_exiting_title">
+        <source>Exit offline mode?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_offl_exiting">
+        <source>Connections will be restored.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_offl_entering">
+        <source>Closing all connections. Switching to offline mode.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- offlineapplet_ru.ts
+++ offlineapplet_ru.ts
@@ -6,32 +6,56 @@
     <message>
         <source>qtn_offl_entering</source>
         <translatorcomment>Closing all connections. Switching to offline mode.</translatorcomment>
-        <translation>Закрываются все соединения. Переключаемся в режим &quot;Вне сети&quot;</translation>
+        <translation type="obsolete">Закрываются все соединения. Переключаемся в режим &quot;Вне сети&quot;</translation>
     </message>
     <message>
         <source>qtn_offl_exiting_title</source>
         <translatorcomment>Exit offline mode?</translatorcomment>
-        <translation>Перейти в режии &quot;Вне сети&quot;?</translation>
+        <translation type="obsolete">Перейти в режии &quot;Вне сети&quot;?</translation>
     </message>
     <message>
         <source>qtn_sett_main_flightmode</source>
         <translatorcomment>Flight mode</translatorcomment>
-        <translation>Режим полёта</translation>
+        <translation type="obsolete">Режим полёта</translation>
     </message>
     <message>
         <source>qtn_offl_exiting</source>
         <translatorcomment>Connections will be restored.</translatorcomment>
-        <translation>Соединения были востановлены.</translation>
+        <translation type="obsolete">Соединения были востановлены.</translation>
     </message>
     <message>
         <source>qtn_offl_deactivate</source>
         <translatorcomment>Deactivate offline mode</translatorcomment>
-        <translation>Отключить режим &quot;Вне сети&quot;</translation>
+        <translation type="obsolete">Отключить режим &quot;Вне сети&quot;</translation>
     </message>
     <message>
         <source>qtn_offl_activate</source>
         <translatorcomment>Activate offline mode</translatorcomment>
+        <translation type="obsolete">Включить режим &quot;Вне сети&quot;</translation>
+    </message>
+    <message id="qtn_sett_main_flightmode">
+        <source>Flight mode</source>
+        <translation>Режим полёта</translation>
+    </message>
+    <message id="qtn_offl_deactivate">
+        <source>Deactivate offline mode</source>
+        <translation>Отключить режим &quot;Вне сети&quot;</translation>
+    </message>
+    <message id="qtn_offl_activate">
+        <source>Activate offline mode</source>
         <translation>Включить режим &quot;Вне сети&quot;</translation>
+    </message>
+    <message id="qtn_offl_exiting_title">
+        <source>Exit offline mode?</source>
+        <translation>Включить режим &quot;Вне сети&quot;?</translation>
+    </message>
+    <message id="qtn_offl_exiting">
+        <source>Connections will be restored.</source>
+        <translation>Соединения были востановлены.</translation>
+    </message>
+    <message id="qtn_offl_entering">
+        <source>Closing all connections. Switching to offline mode.</source>
+        <translation>Закрываются все соединения. Переходим в режим &quot;вне сети&quot;.</translation>
     </message>
 </context>
 </TS>
