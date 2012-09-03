45d2f4ae0c330782ee6ec247e817914ea9c306cc
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 10:30:50 +0400

    Add sound applet files

---
+++ soundsettingsapplet_en.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name></name>
+    <message id="qtn_comm_settings_off">
+        <location filename="alerttoneappletmaps.cpp" line="104"/>
+        <source>Off</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_level_1">
+        <location filename="alerttoneappletmaps.cpp" line="106"/>
+        <source>Level 1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_level_2">
+        <location filename="alerttoneappletmaps.cpp" line="108"/>
+        <source>Level 2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_level_3">
+        <location filename="alerttoneappletmaps.cpp" line="110"/>
+        <source>Level 3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_system">
+        <location filename="alerttoneappletmaps.cpp" line="112"/>
+        <source>System sounds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_keyboard">
+        <location filename="alerttoneappletmaps.cpp" line="114"/>
+        <source>Keyboard feedback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_touch_screen">
+        <location filename="alerttoneappletmaps.cpp" line="116"/>
+        <source>Touch screen tones</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_touch_vibra">
+        <location filename="alerttoneappletmaps.cpp" line="118"/>
+        <source>Touch screen vibration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_ring_tone">
+        <location filename="alerttoneappletmaps.cpp" line="120"/>
+        <source>Ringing tone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_internet_tone">
+        <location filename="alerttoneappletmaps.cpp" line="122"/>
+        <source>Internet tone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_email_tone">
+        <location filename="alerttoneappletmaps.cpp" line="124"/>
+        <source>E-mail tone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_message_tone">
+        <location filename="alerttoneappletmaps.cpp" line="126"/>
+        <source>SMS tone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_ins_messaging">
+        <location filename="alerttoneappletmaps.cpp" line="128"/>
+        <source>Instant messaging tone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_org_reminders">
+        <location filename="alerttoneappletmaps.cpp" line="130"/>
+        <source>Calendar reminder tone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_sounds">
+        <location filename="alerttoneappletwidget.cpp" line="205"/>
+        <location filename="soundsettingsapplet.cpp" line="161"/>
+        <source>Alert tones</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_prof_vibration">
+        <location filename="alerttoneappletwidget.cpp" line="223"/>
+        <source>Vibration profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_event_tones">
+        <location filename="alerttoneappletwidget.cpp" line="236"/>
+        <source>Alert tones</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_feedback">
+        <location filename="alerttoneappletwidget.cpp" line="250"/>
+        <source>Feedback</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_pick_music">
+        <location filename="alerttonebrowser.cpp" line="232"/>
+        <location filename="drilldownitem.cpp" line="173"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_store">
+        <location filename="alerttonebrowser.cpp" line="237"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_ringtones">
+        <location filename="alerttonebrowser.cpp" line="242"/>
+        <source>Ringtones</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_command_done">
+        <location filename="alerttonebrowser.cpp" line="246"/>
+        <location filename="alerttonebrowser.cpp" line="640"/>
+        <location filename="alerttonebrowsersheet.cpp" line="55"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_cancel">
+        <location filename="alerttonebrowser.cpp" line="250"/>
+        <location filename="alerttonebrowser.cpp" line="646"/>
+        <location filename="alerttonebrowsersheet.cpp" line="57"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_prof_ringing">
+        <location filename="profiledatainterface.cpp" line="208"/>
+        <source>Ringing</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_prof_silent">
+        <location filename="profiledatainterface.cpp" line="211"/>
+        <source>Silent</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_prof_beep">
+        <location filename="profiledatainterface.cpp" line="214"/>
+        <source>Beep</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_sond_ringtone_nosound">
+        <location filename="trackerconnection.cpp" line="352"/>
+        <source>No sound</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ soundsettingsapplet_ru.ts
@@ -0,0 +1,148 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message id="qtn_comm_settings_off">
+        <location filename="alerttoneappletmaps.cpp" line="104"/>
+        <source>Off</source>
+        <translation>Выкл</translation>
+    </message>
+    <message id="qtn_sond_level_1">
+        <location filename="alerttoneappletmaps.cpp" line="106"/>
+        <source>Level 1</source>
+        <translation>Уровень 1</translation>
+    </message>
+    <message id="qtn_sond_level_2">
+        <location filename="alerttoneappletmaps.cpp" line="108"/>
+        <source>Level 2</source>
+        <translation>Уровень 2</translation>
+    </message>
+    <message id="qtn_sond_level_3">
+        <location filename="alerttoneappletmaps.cpp" line="110"/>
+        <source>Level 3</source>
+        <translation>Уровень 3</translation>
+    </message>
+    <message id="qtn_sond_system">
+        <location filename="alerttoneappletmaps.cpp" line="112"/>
+        <source>System sounds</source>
+        <translation>Системные звуки</translation>
+    </message>
+    <message id="qtn_sond_keyboard">
+        <location filename="alerttoneappletmaps.cpp" line="114"/>
+        <source>Keyboard feedback</source>
+        <translation>Отклик клавиатуры</translation>
+    </message>
+    <message id="qtn_sond_touch_screen">
+        <location filename="alerttoneappletmaps.cpp" line="116"/>
+        <source>Touch screen tones</source>
+        <translation>Звуки прикосновения к экрану</translation>
+    </message>
+    <message id="qtn_sond_touch_vibra">
+        <location filename="alerttoneappletmaps.cpp" line="118"/>
+        <source>Touch screen vibration</source>
+        <translation>Вибрация при прикосновении к экрану</translation>
+    </message>
+    <message id="qtn_sond_ring_tone">
+        <location filename="alerttoneappletmaps.cpp" line="120"/>
+        <source>Ringing tone</source>
+        <translation>Мелодия звонка</translation>
+    </message>
+    <message id="qtn_sond_internet_tone">
+        <location filename="alerttoneappletmaps.cpp" line="122"/>
+        <source>Internet tone</source>
+        <translation>Мелодия интернета</translation>
+    </message>
+    <message id="qtn_sond_email_tone">
+        <location filename="alerttoneappletmaps.cpp" line="124"/>
+        <source>E-mail tone</source>
+        <translation>Мелодия e-mail</translation>
+    </message>
+    <message id="qtn_sond_message_tone">
+        <location filename="alerttoneappletmaps.cpp" line="126"/>
+        <source>SMS tone</source>
+        <translation>Звук SMS</translation>
+    </message>
+    <message id="qtn_sond_ins_messaging">
+        <location filename="alerttoneappletmaps.cpp" line="128"/>
+        <source>Instant messaging tone</source>
+        <translation>Звук сообщения</translation>
+    </message>
+    <message id="qtn_sond_org_reminders">
+        <location filename="alerttoneappletmaps.cpp" line="130"/>
+        <source>Calendar reminder tone</source>
+        <translation>Звук напоминания календаря</translation>
+    </message>
+    <message id="qtn_sond_sounds">
+        <location filename="alerttoneappletwidget.cpp" line="205"/>
+        <location filename="soundsettingsapplet.cpp" line="161"/>
+        <source>Alert tones</source>
+        <translation>Звук предупреждений</translation>
+    </message>
+    <message id="qtn_prof_vibration">
+        <location filename="alerttoneappletwidget.cpp" line="223"/>
+        <source>Vibration profile</source>
+        <translation>Профиль вибрации</translation>
+    </message>
+    <message id="qtn_sond_event_tones">
+        <location filename="alerttoneappletwidget.cpp" line="236"/>
+        <source>Alert tones</source>
+        <translation>Звук предупреждений</translation>
+    </message>
+    <message id="qtn_sond_feedback">
+        <location filename="alerttoneappletwidget.cpp" line="250"/>
+        <source>Feedback</source>
+        <translation>Отклик</translation>
+    </message>
+    <message id="qtn_sond_pick_music">
+        <location filename="alerttonebrowser.cpp" line="232"/>
+        <location filename="drilldownitem.cpp" line="173"/>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message id="qtn_sond_store">
+        <location filename="alerttonebrowser.cpp" line="237"/>
+        <source></source>
+        <translation></translation>
+    </message>
+    <message id="qtn_sond_ringtones">
+        <location filename="alerttonebrowser.cpp" line="242"/>
+        <source>Ringtones</source>
+        <translation>Мелодии</translation>
+    </message>
+    <message id="qtn_comm_command_done">
+        <location filename="alerttonebrowser.cpp" line="246"/>
+        <location filename="alerttonebrowser.cpp" line="640"/>
+        <location filename="alerttonebrowsersheet.cpp" line="55"/>
+        <source>Done</source>
+        <translation>Готово</translation>
+    </message>
+    <message id="qtn_comm_cancel">
+        <location filename="alerttonebrowser.cpp" line="250"/>
+        <location filename="alerttonebrowser.cpp" line="646"/>
+        <location filename="alerttonebrowsersheet.cpp" line="57"/>
+        <source>Cancel</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message id="qtn_prof_ringing">
+        <location filename="profiledatainterface.cpp" line="208"/>
+        <source>Ringing</source>
+        <translation>Мелодия</translation>
+    </message>
+    <message id="qtn_prof_silent">
+        <location filename="profiledatainterface.cpp" line="211"/>
+        <source>Silent</source>
+        <translation>Тихий</translation>
+    </message>
+    <message id="qtn_prof_beep">
+        <location filename="profiledatainterface.cpp" line="214"/>
+        <source>Beep</source>
+        <translation>Гудок</translation>
+    </message>
+    <message id="qtn_sond_ringtone_nosound">
+        <location filename="trackerconnection.cpp" line="352"/>
+        <source>No sound</source>
+        <translation>Без звука</translation>
+    </message>
+</context>
+</TS>
