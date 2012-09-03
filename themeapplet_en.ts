7eec2189f3763d975b85eac069aa2f742f5c87a0
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 10:34:50 +0400

    Add theme applet files

---
+++ themeapplet_en.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name></name>
+    <message id="qtn_teme_theme">
+        <location filename="themeapplet.cpp" line="85"/>
+        <source>Theme</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_teme_select">
+        <location filename="themedialog.cpp" line="110"/>
+        <source>Select</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_cancel">
+        <location filename="themedialog.cpp" line="115"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_teme_store">
+        <location filename="themewidget.cpp" line="154"/>
+        <source>Get more from Ovi Store</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ themeapplet_ru.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message id="qtn_teme_theme">
+        <location filename="themeapplet.cpp" line="85"/>
+        <source>Theme</source>
+        <translation>Тема</translation>
+    </message>
+    <message id="qtn_teme_select">
+        <location filename="themedialog.cpp" line="110"/>
+        <source>Select</source>
+        <translation>Выбрать</translation>
+    </message>
+    <message id="qtn_comm_cancel">
+        <location filename="themedialog.cpp" line="115"/>
+        <source>Cancel</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message id="qtn_teme_store">
+        <location filename="themewidget.cpp" line="154"/>
+        <source>Get more from Ovi Store</source>
+        <translation>Больше тем на Ovi Store</translation>
+    </message>
+</context>
+</TS>
