1f239d41c1385dc6e8ca2946e6b84680318f2c85
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 10:43:49 +0400

    Add warranty applet files

---
+++ warrantyapplet_en.ts
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name></name>
+    <message id="qtn_warr_title">
+        <location filename="warrantyapplet.cpp" line="74"/>
+        <location filename="warrantywidget.cpp" line="170"/>
+        <source>Warranty</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_warr_expiration" numerus="yes">
+        <location filename="warrantywidget.cpp" line="216"/>
+        <source>Product warranty will expire in &lt;b&gt;%Ln&lt;/b&gt; day.</source>
+        <translation type="unfinished">
+            <numerusform></numerusform>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message id="qtn_warr_expired">
+        <location filename="warrantywidget.cpp" line="220"/>
+        <source>Product warranty is expired.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_warr_error_timer">
+        <location filename="warrantywidget.cpp" line="223"/>
+        <source>Warranty timer error.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_warr_terms">
+        <location filename="warrantywidget.cpp" line="230"/>
+        <source>(insert terms of warranty here)</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ warrantyapplet_ru.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message id="qtn_warr_title">
+        <location filename="warrantyapplet.cpp" line="74"/>
+        <location filename="warrantywidget.cpp" line="170"/>
+        <source>Warranty</source>
+        <translation>Гарантия</translation>
+    </message>
+    <message id="qtn_warr_expiration" numerus="yes">
+        <location filename="warrantywidget.cpp" line="216"/>
+        <source>Product warranty will expire in &lt;b&gt;%Ln&lt;/b&gt; day.</source>
+        <translation>
+            <numerusform>Гарантия закончится через &lt;b&gt;%Ln&lt;/b&gt; день.</numerusform>
+            <numerusform>Гарантия закончится через &lt;b&gt;%Ln&lt;/b&gt; дня.</numerusform>
+            <numerusform>Гарантия закончится через &lt;b&gt;%Ln&lt;/b&gt; дней.</numerusform>
+        </translation>
+    </message>
+    <message id="qtn_warr_expired">
+        <location filename="warrantywidget.cpp" line="220"/>
+        <source>Product warranty is expired.</source>
+        <translation>Гарантия закончилась.</translation>
+    </message>
+    <message id="qtn_warr_error_timer">
+        <location filename="warrantywidget.cpp" line="223"/>
+        <source>Warranty timer error.</source>
+        <translation>Ошибка подсчёта времени гарантии</translation>
+    </message>
+    <message id="qtn_warr_terms">
+        <location filename="warrantywidget.cpp" line="230"/>
+        <source>(insert terms of warranty here)</source>
+        <translation>(вставить текст гарантии сюда)</translation>
+    </message>
+</context>
+</TS>
