0e5843a2e7e80be8b7d577dac01a48d442f72f3f
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 11:34:52 +0400

    Add language files

---
+++ language_en.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name></name>
+    <message id="qtn_comm_settings_language">
+        <location filename="../src/displaylangapplet.cpp" line="64"/>
+        <location filename="../src/displaywidget.cpp" line="141"/>
+        <location filename="../src/displaywidget.cpp" line="303"/>
+        <location filename="../src/displaywidget.cpp" line="576"/>
+        <source>Language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_cancel">
+        <location filename="../src/displaywidget.cpp" line="212"/>
+        <location filename="../src/displaywidget.cpp" line="582"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_command_done">
+        <location filename="../src/displaywidget.cpp" line="213"/>
+        <location filename="../src/displaywidget.cpp" line="581"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_lang_usb_reboot_fail">
+        <location filename="../src/displaywidget.cpp" line="344"/>
+        <source>Language cannot be changed while USB mass storage is in use.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_lang_change_down">
+        <location filename="../src/displaywidget.cpp" line="353"/>
+        <source>Changing language</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_lang_wait">
+        <location filename="../src/displaywidget.cpp" line="355"/>
+        <source>Please wait!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_lang_change_title">
+        <location filename="../src/displaywidget.cpp" line="634"/>
+        <source>Change device language?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_lang_change_query">
+        <location filename="../src/displaywidget.cpp" line="636"/>
+        <source>The device will reboot, temporarily disabling all functions, including emergency calls.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ language_ru.ts
@@ -0,0 +1,52 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message id="qtn_comm_settings_language">
+        <location filename="../src/displaylangapplet.cpp" line="64"/>
+        <location filename="../src/displaywidget.cpp" line="141"/>
+        <location filename="../src/displaywidget.cpp" line="303"/>
+        <location filename="../src/displaywidget.cpp" line="576"/>
+        <source>Language</source>
+        <translation>Язык</translation>
+    </message>
+    <message id="qtn_comm_cancel">
+        <location filename="../src/displaywidget.cpp" line="212"/>
+        <location filename="../src/displaywidget.cpp" line="582"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_comm_command_done">
+        <location filename="../src/displaywidget.cpp" line="213"/>
+        <location filename="../src/displaywidget.cpp" line="581"/>
+        <source></source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="qtn_lang_usb_reboot_fail">
+        <location filename="../src/displaywidget.cpp" line="344"/>
+        <source>Language cannot be changed while USB mass storage is in use.</source>
+        <translation>Язык не может быть изменён когда USB используется в режиме внешнего диска.</translation>
+    </message>
+    <message id="qtn_lang_change_down">
+        <location filename="../src/displaywidget.cpp" line="353"/>
+        <source>Changing language</source>
+        <translation>Изменяются настройки языка</translation>
+    </message>
+    <message id="qtn_lang_wait">
+        <location filename="../src/displaywidget.cpp" line="355"/>
+        <source>Please wait!</source>
+        <translation>Подождите!</translation>
+    </message>
+    <message id="qtn_lang_change_title">
+        <location filename="../src/displaywidget.cpp" line="634"/>
+        <source>Change device language?</source>
+        <translation>Изменить режим языка?</translation>
+    </message>
+    <message id="qtn_lang_change_query">
+        <location filename="../src/displaywidget.cpp" line="636"/>
+        <source>The device will reboot, temporarily disabling all functions, including emergency calls.</source>
+        <translation>Устройство будет перезагружено, временно не будут доступны все функции включая экстренные вызовы.</translation>
+    </message>
+</context>
+</TS>
