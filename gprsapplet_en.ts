28714bbf1a09f84df0459e4b3928694ded1d3b17
Sergey Chupligin <neochapay@gmail.com>
2012-09-03 10:08:08 +0400

    Add gprs applet files

---
+++ gprsapplet_en.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="en_US">
+<context>
+    <name></name>
+    <message id="qtn_prod_gprs_connectivity">
+        <location filename="gprsapplet.cpp" line="83"/>
+        <source>gprs APN</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>gprsWidget</name>
+    <message>
+        <location filename="gprswidget.cpp" line="256"/>
+        <source>Access point name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="273"/>
+        <source>User name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="289"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="321"/>
+        <source>Access Point Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="376"/>
+        <location filename="gprswidget.cpp" line="427"/>
+        <source>Open Connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="380"/>
+        <location filename="gprswidget.cpp" line="436"/>
+        <source>Close Connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ gprsapplet_ru.ts
@@ -0,0 +1,47 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name></name>
+    <message id="qtn_prod_gprs_connectivity">
+        <location filename="gprsapplet.cpp" line="83"/>
+        <source>gprs APN</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>gprsWidget</name>
+    <message>
+        <location filename="gprswidget.cpp" line="256"/>
+        <source>Access point name</source>
+        <translation>Имя точки доступа</translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="273"/>
+        <source>User name</source>
+        <translation>Имя пользователя</translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="289"/>
+        <source>Password</source>
+        <translation>Пароль</translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="321"/>
+        <source>Access Point Settings</source>
+        <translation>Настройки точки доступа</translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="376"/>
+        <location filename="gprswidget.cpp" line="427"/>
+        <source>Open Connection</source>
+        <translation>Открыть соединение</translation>
+    </message>
+    <message>
+        <location filename="gprswidget.cpp" line="380"/>
+        <location filename="gprswidget.cpp" line="436"/>
+        <source>Close Connection</source>
+        <translation>Закрыть соединение</translation>
+    </message>
+</context>
+</TS>
